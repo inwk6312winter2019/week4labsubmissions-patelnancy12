5e9f8b83c2e58fcea75eb4eb64350a6810da3428
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-03 22:06:16 +0000

    Task done

--- l4task1.py
+++ l4task1.py
@@ -1,4 +1,16 @@
 import string
-print(string.punctuation)
 
-print("nancy")
+def stripped_text():
+	n_res = []
+	f = open('file.txt','r')
+
+	for line in f:
+		res = line.lower().strip(string.punctuation + string.whitespace).split()
+		n_res += res
+	return n_res
+
+print(stripped_text())
+
+
+
+
