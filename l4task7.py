4c523ec99acded5f42389b620e779d3910f16e16
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-03 18:46:27 -0400

    task done

--- l4task7.py
+++ l4task7.py
@@ -1,2 +1,24 @@
-print("task7")
+import string
+def removepattern(word):
+    punc = string.punctuation
+    stri = ""
+    for p in punc:
+        stri = word.strip()
+        stri = word.replace(p,'')
+    return stri
 
+def sed(filename1,filename2):
+     try:
+        master = open(filename1,"r")
+        slave = open(filename2,"w")
+        for ma in master:
+            new = removepattern(ma)
+            slave.write(ma)
+        print("File write operation completed successfully :-)")
+     except:
+        print("Oops something went wrong!!!")
+
+
+x = input("Enter master file for copy operation:")
+y = input("Enter slave file:")
+sed(x,y)
